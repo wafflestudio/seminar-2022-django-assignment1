0357d548bc3e7db466c164569d5ca1e62f5a0caf
Jinho Choi <dandyday31@gmail.com>
2022-09-25 18:07:07 +0900

    pr3 clear

--- problem-2/waffleBlog/blog/urls.py
+++ problem-2/waffleBlog/blog/urls.py
@@ -1,24 +1,29 @@
-from django.urls import path
-from blog.views import PostViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 
-post_list = PostViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
+# from blog.views import PostViewSet
 
-post_detail = PostViewSet.as_view({
-    'get': 'retrieve',
-    'post': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
+# post_list = PostViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+#
+# post_detail = PostViewSet.as_view({
+#     'get': 'retrieve',
+#     'post': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet, basename='post')
 
 app_name = 'blog'
 urlpatterns = [
-    path('posts/', post_list, name='list'),
-    path('posts/<int:pk>/', post_detail, name='detail')
-    # path('', views.IndexView.as_view(), name='index'),
-    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('posts/', post_list, name='list'),
+    # path('posts/<int:pk>/', post_detail, name='detail'),
+    path('v1/', include(router.urls)),
+    path('v2/posts/', views.get_post_list),
+    path('v2/posts/<int:pk>/', views.get_post_detail)
 ]
--- problem-2/waffleBlog/blog/views.py
+++ problem-2/waffleBlog/blog/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from blog.models import Post
 from blog.serializers import PostSerializer
 
@@ -6,3 +9,40 @@ from blog.serializers import PostSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+@api_view(['GET', 'POST'])
+def get_post_list(request):
+    queryset = Post.objects.all()
+    serializer = PostSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE', 'GET', 'PATCH', 'PUT'])
+def get_post_detail(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PATCH':
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
