9e1f66ba3253d568f398191b435cf99c34f6ff16
LimSusu <dlatn4389@snu.ac.kr>
2022-09-28 02:21:45 +0900

    problem2 start

--- problem-2/waffleBlogProject/waffleBlogProject/settings.py
+++ problem-2/waffleBlogProject/waffleBlogProject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'waffleBlogProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'myusername',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
