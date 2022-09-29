db329d663c215e9c943a18951dd6917a611892c3
sungmin <sungsung718@gmail.com>
2022-09-29 21:19:44 +0900

    model 수정

--- problem-2/waffleBlogProject/blog/models.py
+++ problem-2/waffleBlogProject/blog/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50, blank=True)
+    author_photo = models.ImageField(upload_to='upload_photo/', default='default.jpg')
+    article_photo = models.ImageField(upload_to='upload_image/', blank=True, null=True)
+    author_email = models.EmailField(blank=True)
     description = models.TextField()
     date_published = models.DateTimeField(default=timezone.now())
-    author_email = models.EmailField(blank=True)
+    reading_time = models.DurationField(blank=True, null=True)
+
+
+
+
 
 
 
