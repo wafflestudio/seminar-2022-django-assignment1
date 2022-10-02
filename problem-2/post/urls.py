13bbdb752a5b8cca03e11a4a9e45496da6d8b5af
reynie1514 <reynie1514@naver.com>
2022-10-02 13:28:13 +0900

    Problem 3 FBV Solved

--- problem-2/post/urls.py
+++ problem-2/post/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import PostViewSet
+from .views import PostViewSet, postsAPI, postAPI
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v2/posts/', postsAPI),
+    path('v2/posts/<int:id>/', postAPI),
 ]
--- problem-2/post/views.py
+++ problem-2/post/views.py
@@ -1,9 +1,49 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 
 from .models import Post
 from .serializers import PostSerializer
 # Create your views here.
 
+# Class Based View
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+# Function Based View
+@api_view(['GET', 'POST'])
+def postsAPI(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def postAPI(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PATCH':
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
