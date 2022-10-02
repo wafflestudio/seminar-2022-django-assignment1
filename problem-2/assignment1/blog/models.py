65d29671c3617c0c99fcedf408f779b1ca5d9b60
bean109 <auddl1020@gmail.com>
2022-10-02 18:03:00 +0900

    problem2,3 model 수정

--- problem-2/assignment1/blog/models.py
+++ problem-2/assignment1/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
+    author = models.CharField(max_length=50, default='')
     contents = models.TextField()
     email = models.EmailField(max_length=100)
 
