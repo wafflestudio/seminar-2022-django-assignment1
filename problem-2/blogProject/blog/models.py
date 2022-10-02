e83b54452ecb220b074a59af44ff0204051ad64a
catnip033 <henry11n@naver.com>
2022-10-02 18:02:13 +0900

    improved post model

--- problem-2/blogProject/blog/models.py
+++ problem-2/blogProject/blog/models.py
@@ -4,8 +4,10 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    reading_time = models.PositiveIntegerField(null=True, blank=True)
+    author = models.CharField(max_length=50, blank=True, default='')
     created = models.DateTimeField(auto_now_add=True)
-    email = models.EmailField(max_length=50)
+    email = models.EmailField(max_length=50, null=True, blank=True)
     description = models.TextField()
 
     class Meta:
