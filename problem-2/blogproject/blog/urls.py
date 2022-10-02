348b9c6487aeb1d314e9ab0a7b83a94b68130fb0
BDlhj <akk808@naver.com>
2022-10-02 17:27:49 +0900

    Finish problem 3

--- problem-2/blogproject/blog/urls.py
+++ problem-2/blogproject/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import post_list, post_detail
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -9,4 +10,6 @@ router.register(r'posts', views.PostViewSet, basename="post")
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v2/posts/', post_list),
+    path('v2/posts/<int:post_id>/', post_detail),
 ]
